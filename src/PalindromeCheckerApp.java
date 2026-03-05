c3d12377880c67013289615a41bdb9f14100c1ec
Sanyam Raj Singh Dodiya <sanyamrajsingh@gmail.com>
2026-03-05 09:05:43 +0530

    UC11 updated

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,3 +1,35 @@
-public class PalindromeCheckerApp {
+class PalindromeChecker {
+
+    public boolean checkPalindrome(String input, int start, int end) {
+        if (start >= end) {
+            return true;
+        }
+
+        if (input.charAt(start) != input.charAt(end)) {
+            return false;
+        }
+
+        return checkPalindrome(input, start + 1, end - 1);
+    }
 }
-/////////he;;llpkfe
+
+public class UseCase11PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String input = "a man a plan a canal panama";
+
+        String lowerCaseInput = input.toLowerCase();
+        String noSpace = lowerCaseInput.replaceAll("\\s", "");
+
+        PalindromeChecker checker = new PalindromeChecker();
+
+        boolean result = checker.checkPalindrome(noSpace, 0, noSpace.length() - 1);
+
+        System.out.println("UC11 Input: " + input);
+        System.out.println("LowerCase converted Input: " + lowerCaseInput);
+        System.out.println("NoSpace converted Input: " + noSpace);
+        System.out.println("Palindrome: " + result + "\n");
+
+    }
+}
